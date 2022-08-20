49312807633ff1992264b32123efff4c275b354e
Helen <elenaprofessor@yandex.ru>
2022-08-20 17:08:17 +0300

    2 задача

---
+++ Задача2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Нахождение максимального из 3 числел
+int a = -9;
+int b = -3;
+int с = 6;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (с > max) max = с;
+Console.WriteLine(max);
